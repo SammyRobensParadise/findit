f826d979ffbbb5d31cdff35772d82b50c4eeff45
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-05-09 13:25:12 -0400

    added render styles

--- components/UserSignIn.tsx
+++ components/UserSignIn.tsx
@@ -1,19 +1,10 @@
-import { Anchor, Box, Button, Card, Footer, Text } from 'grommet'
-import Image from 'next/image'
-
-import Logo from '../public/pharmabox_logo.svg'
-import { useEffect, useState } from 'react'
+import { Box, Button, Card, CardBody, CardHeader, Footer, Text } from 'grommet'
 import { useClerk } from '@clerk/nextjs'
 import { FaceAdd, Login } from '@carbon/icons-react'
-import Link from 'next/link'
 
 export default function UserSignIn() {
-  const [, setLoaded] = useState<boolean>(false)
-
-  useEffect(() => setLoaded(true), [])
-
   return (
-    <Box className="moving-gradient" fill>
+    <Box fill>
       <Box
         align="center"
         pad="large"
@@ -34,26 +25,29 @@ const SignedOutCards = () => {
   const { openSignIn, openSignUp } = useClerk()
   return (
     <Card pad="large" gap="medium" align="center" width="medium">
-      <Box align="center" gap="xsmall">
-        <Button
-          icon={<Login size={20} />}
-          fill="horizontal"
-          primary
-          onClick={() => openSignIn()}
-          label="Sign In"
-          style={{ borderRadius: '24px' }}
-          // when user press enter trigger the login overlay
-          autoFocus
-        />
-        <Text size="xsmall">Or</Text>
-        <Button
-          icon={<FaceAdd size={20} />}
-          secondary
-          label="Sign Up"
-          onClick={() => openSignUp()}
-          style={{ borderRadius: '24px' }}
-        />
-      </Box>
+      <CardHeader>
+        <Text size="medium" weight="bolder">
+          The Findit App
+        </Text>
+      </CardHeader>
+      <CardBody gap="small">
+        <Box align="center" gap="xsmall">
+          <Button
+            icon={<Login size={16} />}
+            primary
+            onClick={() => openSignIn()}
+            label="Sign In"
+            autoFocus
+          />
+          <Text size="xsmall">or</Text>
+          <Button
+            icon={<FaceAdd size={16} />}
+            secondary
+            label="Sign Up"
+            onClick={() => openSignUp()}
+          />
+        </Box>
+      </CardBody>
     </Card>
   )
 }
