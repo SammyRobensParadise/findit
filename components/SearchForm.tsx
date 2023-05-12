dec92e369b09c568e3856491bc1b19aa647298eb
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-05-12 12:34:38 -0400

    added search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,8 +1,16 @@
 import { useKeywords } from '@/hooks/keywords'
 import { UserWithCollections } from '@/types'
-import { Collection } from '@prisma/client'
-import { Form, FormField, Select } from 'grommet'
-import { useState } from 'react'
+import { Collection, Keyword } from '@prisma/client'
+import {
+  Form,
+  FormField,
+  Select,
+  SelectMultiple,
+  Box,
+  Text,
+  Tag
+} from 'grommet'
+import { useEffect, useState } from 'react'
 
 export default function SearchForm({ user }: { user: UserWithCollections }) {
   const [collection, setCollection] = useState<Partial<Collection>>()
@@ -10,7 +18,14 @@ export default function SearchForm({ user }: { user: UserWithCollections }) {
     userId: user.id,
     collectionId: collection?.id
   })
-  console.log(keywords)
+  const [valueMultiple, setValueMultiple] = useState<Keyword[]>([])
+  const [options, setOptions] = useState(keywords)
+
+  useEffect(() => {
+    setOptions(keywords)
+  }, [keywords])
+
+  // console.log(options)
   return (
     <Form>
       <FormField name="collection" label="Collection" htmlFor="collection">
@@ -22,9 +37,36 @@ export default function SearchForm({ user }: { user: UserWithCollections }) {
         />
       </FormField>
       {keywords?.length && (
-        <FormField name="keywords" label="Keywords" htmlFor="keywords">
-          <Select options={keywords} name="keywords" id="keywords" multiple />
-        </FormField>
+        <Box>
+          <FormField name="keywords" label="Keywords" htmlFor="keywords">
+            <Box pad="small" direction="row" gap="small">
+              {valueMultiple.map((value: Keyword) => (
+                <Tag
+                  size="small"
+                  name="Keyword"
+                  value={value.name}
+                  key={value.id}
+                />
+              ))}
+            </Box>
+            <SelectMultiple
+              showSelectedInline
+              size="medium"
+              placeholder="Select multiple options"
+              value={valueMultiple}
+              options={options ?? []}
+              onChange={({ value: nextValue }) => setValueMultiple(nextValue)}
+              onClose={() => setOptions(keywords)}
+              onSearch={(text) => {
+                const escapedText = text.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&')
+                const exp = new RegExp(escapedText, 'i')
+                const newOptions = keywords?.filter((o) => exp.test(o.name))
+                console.log(newOptions)
+                setOptions(newOptions)
+              }}
+            />
+          </FormField>
+        </Box>
       )}
     </Form>
   )
