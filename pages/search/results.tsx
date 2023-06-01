de1d30a3cd0c5a48aadca3b02e22bf1eb62eb76c
Sammy Robens-Paradise <srobensparadise@gmail.com>
2023-06-01 20:24:20 -0300

    added data

--- pages/search/results.tsx
+++ pages/search/results.tsx
@@ -29,6 +29,29 @@ export default function Results(props: {
   } = props
 
   const router = useRouter()
+
+  if (!items.length) {
+    return (
+      <>
+        <Head>
+          <title>Findit | Not Found</title>
+          <meta
+            name="description"
+            content="Findit Home Page, Search or Create"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Page user={user}>
+          <Text>
+            The items you are looking for does not exist or has been deleted. If
+            you think this is an error, contact your system administrator. 😢
+          </Text>
+        </Page>
+      </>
+    )
+  }
+
   return (
     <>
       <Head>
@@ -41,69 +64,71 @@ export default function Results(props: {
         <Box gap="medium">
           <Text>Results</Text>
           <Box fill="vertical" border={{ side: 'bottom' }}>
-            <DataTable
-              a11yTitle="Items Table"
-              size="medium"
-              fill
-              pin
-              border={{
-                header: 'bottom',
-                body: {
-                  color: 'light-2',
-                  side: 'bottom'
-                }
-              }}
-              pad="small"
-              columns={[
-                {
-                  property: 'name',
-                  header: <Text size="small">Name</Text>,
-                  primary: true,
-                  search: true,
-                  align: 'start',
-                  render: ({ name }) => <Text size="small">{name}</Text>,
-                  footer: (
-                    <Text size="small" color="dark-2">
-                      {items?.length} matching items found in {collectionName}
-                    </Text>
-                  )
-                },
-                {
-                  property: 'description',
-                  header: <Text size="small">Description</Text>,
-                  search: true,
-                  render: ({ description }) => (
-                    <Text size="small">{description}</Text>
-                  )
-                },
-                {
-                  property: 'updatedAt',
-                  header: <Text size="small">Updated</Text>,
-                  render: ({ updatedAt }) => (
-                    <Text size="small">
-                      {updatedAt &&
-                        new Date(updatedAt).toLocaleDateString('en-US')}
-                    </Text>
-                  ),
-                  size: 'small',
-                  align: 'end'
-                },
-                {
-                  property: 'createdAt',
-                  header: <Text size="small">Created</Text>,
-                  render: ({ createdAt }) => (
-                    <Text size="small">
-                      {createdAt &&
-                        new Date(createdAt).toLocaleDateString('en-US')}
-                    </Text>
-                  ),
-                  size: 'xsmall',
-                  align: 'end'
-                }
-              ]}
-              data={items}
-              onClickRow={({ datum }) => router.push(`/item/${datum.id}`)}
-            />
+            {items.length && (
+              <DataTable
+                a11yTitle="Items Table"
+                size="medium"
+                fill
+                pin
+                border={{
+                  header: 'bottom',
+                  body: {
+                    color: 'light-2',
+                    side: 'bottom'
+                  }
+                }}
+                pad="small"
+                columns={[
+                  {
+                    property: 'name',
+                    header: <Text size="small">Name</Text>,
+                    primary: true,
+                    search: true,
+                    align: 'start',
+                    render: ({ name }) => <Text size="small">{name}</Text>,
+                    footer: (
+                      <Text size="small" color="dark-2">
+                        {items?.length} matching items found in {collectionName}
+                      </Text>
+                    )
+                  },
+                  {
+                    property: 'description',
+                    header: <Text size="small">Description</Text>,
+                    search: true,
+                    render: ({ description }) => (
+                      <Text size="small">{description}</Text>
+                    )
+                  },
+                  {
+                    property: 'updatedAt',
+                    header: <Text size="small">Updated</Text>,
+                    render: ({ updatedAt }) => (
+                      <Text size="small">
+                        {updatedAt &&
+                          new Date(updatedAt).toLocaleDateString('en-US')}
+                      </Text>
+                    ),
+                    size: 'small',
+                    align: 'end'
+                  },
+                  {
+                    property: 'createdAt',
+                    header: <Text size="small">Created</Text>,
+                    render: ({ createdAt }) => (
+                      <Text size="small">
+                        {createdAt &&
+                          new Date(createdAt).toLocaleDateString('en-US')}
+                      </Text>
+                    ),
+                    size: 'xsmall',
+                    align: 'end'
+                  }
+                ]}
+                data={items}
+                onClickRow={({ datum }) => router.push(`/item/${datum.id}`)}
+              />
+            )}
           </Box>
         </Box>
       </Page>
