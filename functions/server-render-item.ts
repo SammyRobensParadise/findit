bfb0c2f70b8b2faf3cd5024f25888a6aa6f26fca
Sammy Robens-Paradise <srobensparadise@gmail.com>
2024-01-12 20:46:24 -0800

    updated keyword management

--- functions/server-render-item.ts
+++ functions/server-render-item.ts
@@ -5,10 +5,20 @@ export async function serverRenderItem(
   itemId: number,
   user: UserWithCollections
 ) {
-  const item = await prisma.item.findUnique({
+  let item = await prisma.item.findUnique({
     where: { id: itemId },
     include: { Collection: { include: { users: true } }, keywords: true }
   })
+
+  const itemKeywords = await prisma.keyword.findMany({
+    where: {
+      itemId: item?.id
+    }
+  })
+
+  if (item) {
+    item.keywords = itemKeywords
+  }
   if (item?.Collection?.users?.map((u) => u.id).includes(user.id)) {
     return { props: { item: JSON.parse(JSON.stringify(item)) } }
   }
--- pages/item/[itemId]/edit.tsx
+++ pages/item/[itemId]/edit.tsx
@@ -52,6 +52,7 @@ export default function EditItem(props: {
     userId: user.id,
     collectionId: item.collectionId as string
   })
+  console.log(item)
   const [keywordsValues, setKeywordsValues] = useState<Keyword[]>(
     item.keywords.map((keyword) => ({
       name: keyword.name,
